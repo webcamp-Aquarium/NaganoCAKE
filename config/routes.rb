6b5cb320fd96e8c438bfb317321498047baf5c4c
k-saunagi <k5.daha55@gmail.com>
2020-06-16 15:49:52 +0900

    push前に一応

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :customers
+  resources :customers,only: [:show,:edit,:update]
   devise_for :administrators
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#top'
+
 end
