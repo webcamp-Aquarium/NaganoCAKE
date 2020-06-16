d67244b29219c28a8a9e0d6c44dfb8ccf128df85
k-saunagi <k5.daha55@gmail.com>
2020-06-16 23:00:59 +0900

    会員登録とログイン機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kanji,
+                                              :first_name_kanji,
+                                              :family_name_kane,
+                                              :first_name_kana,
+                                              :postal_code,
+                                              :address,
+                                              :phone,])
+  end
 end
