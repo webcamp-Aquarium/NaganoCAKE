7e477c58adf2e7fe5e854999ce6e54c7783bb7f2
k-saunagi <k5.daha55@gmail.com>
2020-06-16 12:05:24 +0900

    Customersコントローラの作成、ルーティングの設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :customers,only: [:show,:edit,:update]
+  get 'customers/:id/leave' => 'customers#leave'
+  get 'customers/:id/hide' => 'customers#hide'
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/customers_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class CustomersControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get customers_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get customers_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get customers_update_url
+    assert_response :success
+  end
+
+  test "should get leave" do
+    get customers_leave_url
+    assert_response :success
+  end
+
+  test "should get hide" do
+    get customers_hide_url
+    assert_response :success
+  end
+
+end
