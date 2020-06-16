938a0aac38709dd34b914c617d0074a4d33ec91e
k-saunagi <k5.daha55@gmail.com>
2020-06-17 01:43:47 +0900

    カスタマー編集ページ、退会機能no 実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,14 +5,35 @@ class CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.save(customer_params)
   end
 
   def leave
   end
 
   def hide
+    @customer = Customer.find_by(id: params[:id])
+    if @customer.update(is_status: false)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:family_name_kanji,
+                                              :first_name_kanji,
+                                              :family_name_kana,
+                                              :first_name_kana,
+                                              :postal_code,
+                                              :address,
+                                              :phone,
+                                              :is_status)
   end
 end
