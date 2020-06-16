16b42c944ec8b6e6cdf24db19b197c88b5fe04fd
k-saunagi <k5.daha55@gmail.com>
2020-06-16 12:41:08 +0900

    Admin:custoersのコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   resources :customers,only: [:show,:edit,:update]
   get 'customers/:id/leave' => 'customers#leave'
-  get 'customers/:id/hide' => 'customers#hide'
+  # PATCH 'customers/:id/hide' => 'customers#hide'
   devise_for :customers
+
+  resources :cart_items, only: [:index,:create,:destroy,:update]
+  # PATCH 'cart_items' => 'cart_items#reset'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
