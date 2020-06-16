3cffd370042ccd2afd8896302a4f6cf21ea8f3dd
k-saunagi <k5.daha55@gmail.com>
2020-06-17 00:23:24 +0900

    マイページを実装しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kanji,
                                               :first_name_kanji,
-                                              :family_name_kane,
+                                              :family_name_kana,
                                               :first_name_kana,
                                               :postal_code,
                                               :address,
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
+
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
