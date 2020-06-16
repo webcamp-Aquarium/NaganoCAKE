deee9458a6a445b7eb8168dc780aeda060228760
k-saunagi <k5.daha55@gmail.com>
2020-06-16 15:10:22 +0900

    オーダーテーブルのカラム追加とシッピングモデルの作成

--- db/migrate/20200616025459_create_orders.rb
+++ db/migrate/20200616025459_create_orders.rb
@@ -1,6 +1,15 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
+      t.integer :customer_id
+      # 1.入金待ち　2.入金確認　3.製作中　4.発送準備中　5.発送済み
+      t.integer :status,null: false,default: 1
+      t.integer :payment
+      t.string :potal_code
+      t.string :address
+      t.string :name
+      t.integer :total_fee
+      t.integer :delivery_fee,default: 800
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_06_16_060236) do
 
-ActiveRecord::Schema.define(version: 2020_06_16_023457) do
+  create_table "administrators", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_administrators_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_administrators_on_reset_password_token", unique: true
+  end
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "customer_id"
@@ -20,7 +31,6 @@ ActiveRecord::Schema.define(version: 2020_06_16_023457) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,6 +50,37 @@ ActiveRecord::Schema.define(version: 2020_06_16_023457) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_status", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "status", default: 1, null: false
+    t.integer "payment"
+    t.string "potal_code"
+    t.string "address"
+    t.string "name"
+    t.integer "total_fee"
+    t.integer "delivery_fee", default: 800
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.text "description"
+    t.string "image_id"
+    t.integer "price"
+    t.boolean "is_status", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
