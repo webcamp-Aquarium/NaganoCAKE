f5d2352099b3b358af9371ce550bcc83535723f6
horiuchi67 <1234mhoriuchi@gmail.com>
2020-06-16 16:28:39 +0900

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
   devise_for :customers
   resources :customers,only: [:show,:edit,:update]
-  devise_for :administrators
+  get '/customers/:id/leave' => "customers#leave"
+  patch '/customers/:id/hide' => "customers#hide"
+  resources :cart_items,only: [:index,:create,:destroy,:update]
+  delete '/cart_items' => "cart_items#reset"
+  resources :products,only: [:index,:show]
+  resources :shippings,only: [:new,:create,:edit,:update,:destroy]
+  resources :orders,only: [:new,:create,:index,:show]
+  get '/orders/confirm' => "orders#confirm"
+  get '/orders/complete' => "orders#complete"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#top'
+  namespace :admin do
+  	get '/' => "home#top"
+  	devise_for :administrators
+  	resources :products,only: [:index,:new,:create,:show,:edit,:update]
+  	resources :genres,only: [:index,:create,:edit,:update]
+  	resources :customers,only: [:index,:show,:edit,:update]
+  	resources :orders,only: [:index,:show,:update]
+  	resources :order_details,only: [:update]
+  end
+
 end
