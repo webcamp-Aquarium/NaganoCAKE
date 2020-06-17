c6912586de5eab3c91ffb495190250e5b7d82fa5
horiuchi67 <1234mhoriuchi@gmail.com>
2020-06-17 19:04:23 +0900

    配送先新規と編集画面やりました。

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,2 +1,35 @@
 class ShippingsController < ApplicationController
+	def new
+		@shipping = Shipping.new
+		@shippings = Shipping.all
+	end
+	def create
+		@shipping = Shipping.new(shipping_params)
+		@shipping.customer_id = current_user.id
+		@shipping.save
+	end
+
+	def edit
+		@shipping = Shipping.find(params[:id])
+	end
+	def update
+		@shipping = Shipping.find(params[:id])
+		if @shipping.update(shipping_params)
+			redirect_to new_shipping_path
+			#引数に(@shipping)とかを入れるのでしょうか？
+		else
+			render 'edit'
+		end
+	end
+	def destroy
+		@shipping = Shipping.find(params[:id])
+		@shipping.destroy
+		redirect_to new_shipping_path
+	end
+
+	private
+
+	def shipping_params
+		params.require(:shipping).permit(:postal_code,:address,:name)
+	end
 end
