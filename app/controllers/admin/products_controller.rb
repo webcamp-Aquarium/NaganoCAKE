6e5cb9679b09674b367c67776e0480505a4fff41
Taya Kyouhei <taya_mary919@icloud.com>
2020-06-18 17:56:58 +0900

    管理者側の商品新規登録画面作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,35 @@
 class Admin::ProductsController < ApplicationController
+	def new
+		@product = Product.new
+	end
+	
+	def create
+	    @product = Product.new(product_params)
+	    if  @product.save
+	    　　flash[:notice] = 'You have created product successfully.'
+	        redirect_to admin_products_path
+	    else
+	        render 'new'
+	    end
+	end
+
+	def index
+	    @products = Product.all
+	    @product = Product.new
+	end
+	
+	def edit
+	end
+
+	def update
+	end
+	
+	def show
+	end
+
+	private
+    def product_params
+        params.require(:product).permit(:name, :description, :image, :price, :is_status)
+    end 	
+
 end
