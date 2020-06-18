e6595f333d591bb550114bcc78545d4c754130a8
k-saunagi <k5.daha55@gmail.com>
2020-06-18 17:18:14 +0900

    管理者側から見たカスタマー編集画面の完成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,12 +8,26 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customers_path
   end
 
-  def full_name(resource)
-    resource.family_name_kanji + resource.first_name_kanji
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name_kanji,
+                                              :first_name_kanji,
+                                              :family_name_kana,
+                                              :first_name_kana,
+                                              :email,
+                                              :postal_code,
+                                              :address,
+                                              :phone,
+                                              :is_status)
   end
 end
