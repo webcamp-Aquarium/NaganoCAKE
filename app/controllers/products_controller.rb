1560ab40a894ce6086ff26b9508c5fcbab5f0b6f
k-saunagi <k5.daha55@gmail.com>
2020-06-18 20:06:32 +0900

    カスタマー側の商品詳細画面。カート追加ボタンはブランチ切ってそっちで作ります。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,11 @@ class ProductsController < ApplicationController
     @products = Product.all
     @genres = Genre.where(is_status: true)
 	end
-	
+
 	def show
-	end	
+    @genres = Genre.where(is_status: true)
+    @product = Product.find(params[:id])
+    @cart_item = CartItem.new(customer_id: current_customer.id,
+                              product_id: params[:id])
+	end
 end
