bc6eaf3d83015bf20cbd06a8191a8a18cd1af6c8
k-saunagi <k5.daha55@gmail.com>
2020-06-18 16:49:50 +0900

    管理者側の会員詳細ページ

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -2,4 +2,7 @@ module CustomersHelper
   def kanji_full_name(customer)
     "#{customer.family_name_kanji}#{customer.first_name_kanji}"
   end
+  def kana_full_name(customer)
+    "#{customer.family_name_kana}#{customer.first_name_kana}"
+  end
 end
