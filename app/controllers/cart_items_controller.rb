2e423d332ab7d9619daecd80142f2096734db359
k-saunagi <k5.daha55@gmail.com>
2020-06-19 11:50:46 +0900

    カートに追加する機能の実装

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,6 +4,15 @@ class CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(product_id: params[:product_id],
+                                                number: params[:cart_item][:number])
+    if @cart_item.save
+      redirect_to root_path
+    else
+      @genres = Genre.where(is_status: true)
+      @products = Product.find(params[:product_id])
+      render 'products/show'
+    end
   end
 
   def destroy
@@ -13,6 +22,16 @@ class CartItemsController < ApplicationController
   end
 
   def reset
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id,:product_id,:number)
+  end
+
+  def number_params
+    params.require(:number).permit(:number)
   end
 
 end
