06e8b373264071bed0aaf14c8425d7ed3ce1ddba
k-saunagi <k5.daha55@gmail.com>
2020-06-20 21:50:59 +0900

    税込金額,合計金額,小計金額のヘルパーを作成及び編集.

--- app/helpers/cart_items_helper.rb
+++ app/helpers/cart_items_helper.rb
@@ -1,9 +1,13 @@
 module CartItemsHelper
-  def total_fee(resources)
+  def total_fee(resources) #複数のカートアイテムの配列を入れる
     fee = 0
     resources.each do |r|
-      fee += tax_in_price(r.product)*r.number
+      fee += (tax_in_price(r.product).to_i * r.number)
     end
-    return fee
+    return fee.to_s(:delimited)
+  end
+
+  def subtotal_fee(resource) #一種類のカートアイテムを入れる
+    (tax_in_price(resource.product).to_i * resource.number).to_s(:delimited)
   end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,5 @@
 module ProductsHelper
   def tax_in_price(product)
-    (product.price*1.1).round
+    (product.price*1.1).round.to_s(:delimited)
   end
 end
