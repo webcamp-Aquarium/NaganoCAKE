2440db61497a896c0bcb46963a17b3a09478020d
Taya Kyouhei <taya_mary919@icloud.com>
2020-06-22 08:20:50 +0900

    管理者注文履歴製作中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,16 @@
 class Admin::OrdersController < ApplicationController
+	def index
+		@orders = Order.all
+		@order = Order.new(order_params)
+	end
+	
+	def show
+		@order = Order.find(params[:id])
+	end
+
+	private
+    def order_params
+        params.permit(:status,:payment,:postal_code,:address,:name,:total_fee,:delivary_fee,:order_details_id,:customer_id)
+    end     
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,5 @@
 class OrderDetail < ApplicationRecord
 	belongs_to :order
 	belongs_to :product
+	enum :production_status {着手不可: 1, 製作待ち: 2, 製作中: 3, 製作完了: 4}
 end
