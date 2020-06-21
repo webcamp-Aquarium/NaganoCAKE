c6cce1f2968ae7b779cfd96cab1d4b91dbfa70aa
k-saunagi <k5.daha55@gmail.com>
2020-06-21 12:02:05 +0900

    ヘルパーを編集

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -3,15 +3,15 @@ module OrdersHelper
     "#{resource.postal_code}\s#{resource.address}\s#{kanji_full_name(resource)}"
   end
 
-  def shipping_address_array(resource)
-    [resource.postal_code,resource.address,resource.name]
+  def shipping_address(resource)
+    [resource.postal_code,resource.address,resource.name].join(' ')
   end
 
   def shipping_array(customer)
     shippings_of_customer = []
     shippings = Shipping.where(customer_id: customer.id)
     shippings.each do |s|
-      shippings_of_customer << shipping_address_array(s)
+      shippings_of_customer << shipping_address(s)
     end
     shippings_of_customer
   end
