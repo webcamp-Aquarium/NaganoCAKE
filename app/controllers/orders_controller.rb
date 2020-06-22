800033d9c32b6ca94c52e1897a07a78e18d8a26f
Sokomoto <wdws2342319@gmail.com>
2020-06-22 12:32:51 +0900

    注文履歴1回目

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,8 +13,31 @@ class OrdersController < ApplicationController
 	end
 
 	def index
+		@orders = Order.where(customer_id: current_customer.id)
+		@order = Order.new
 	end
 
 	def show
+		@order = Order.find(params[:id])
+		order_history = @order.order_details.new
+		order_history.customer_id = current_customer.id
+
+		if current_customer.order_details.exists?(order_id: "#{params[:id]}")
+			old_order_history = current_customer.order_details.find_by(order_id: "#{params[:id]}")
+			old_order_history.destroy
+		end
+		order_history.save
+
+		historys_stock_limit = 20
+		historys = current_customer.order_details.all
+		if historys.count > historys_stock_limit
+			historys[0].destroy
+		end
+	end
+
+	private
+
+	def order_params
+		params.require(:order).permit(:postal_code, :address, :name, :payment).merge(customer_id: current_customer.id)
 	end
 end
