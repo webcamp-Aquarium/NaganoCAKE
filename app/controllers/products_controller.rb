16552ffda1c2504873667a662fee3ec1cd2484cd
k-saunagi <k5.daha55@gmail.com>
2020-06-22 12:40:58 +0900

    ジャンル検索画面を作成しました。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,27 @@
 class ProductsController < ApplicationController
-	def index
-    @products = Product.where(is_status: true)
-    @genres = Genre.where(is_status: true)
+    before_action :valid_genre
+
+    def index
+        @products = Product.where(is_status: true)
 	end
 
 	def show
-    @genres = Genre.where(is_status: true)
-    @product = Product.find(params[:id])
-    unless @product.is_status
-      redirect_to products_path
-    end
-    @cart_item = CartItem.new
+        @product = Product.find(params[:id])
+      unless @product.is_status
+        redirect_to products_path
+      end
+        @cart_item = CartItem.new
 	end
+
+    def search
+        @products = Product.where(genre_id: params[:genre_id])
+        @genre = Genre.find(params[:genre_id])
+    end
+
+    private
+
+    def valid_genre
+        @genres = Genre.where(is_status: true)
+    end
+
 end
