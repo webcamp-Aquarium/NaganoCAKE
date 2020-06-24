2665bf7d0bf456593ecc76ed115e5d380d69da5e
k-saunagi <k5.daha55@gmail.com>
2020-06-24 22:10:41 +0900

    管理者側TOPへの当日注文件数表示を追加しました

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,6 +1,7 @@
 class Admin::HomeController < ApplicationController
 	before_action :authenticate_admin_administrator!
 	def top
+    @orders = Order.where(created_at: Time.zone.now.all_day)
 	end
-		
+
 end
