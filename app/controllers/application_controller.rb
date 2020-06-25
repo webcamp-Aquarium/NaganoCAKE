b5538dd3504b416a4994df8eb35c73ce42b038d4
horiuchi67 <1234mhoriuchi@gmail.com>
2020-06-25 16:06:59 +0900

    管理者ログアウト後にサインアップ画面に遷移するように設定しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
   def after_sign_in_path_for(resource)
     case resource
     when Customer
@@ -9,9 +11,14 @@ class ApplicationController < ActionController::Base
       admin_path
     end
   end
-
-  protected
-
+  def after_sign_out_path_for(resource)
+    case resource
+    when :customer
+      root_path
+    when :admin_administrator
+      new_admin_administrator_session_path
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kanji,
