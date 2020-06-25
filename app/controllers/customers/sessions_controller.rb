6336c0ea5c3c306e820f42c4bc0eb8caadf7b55f
k-saunagi <k5.daha55@gmail.com>
2020-06-25 14:38:04 +0900

    退会済みカスタマーをログイン不可にしました

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Customers::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+    # before_action :reject_customer,only: :create
   # GET /resource/sign_in
   # def new
   #   super
@@ -23,5 +23,11 @@ class Customers::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  private
+
+  # def reject_customer
+  #   @customer.active_for_authentication?
   # end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,4 +9,12 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   enum is_status: {有効: true, 退会済: false}
+
+  def active_for_authentication?
+    super && (self.is_status == true)
+  end
+
+  def inactive_message
+    self.is_status == true ? super : :customer_status_is_not_valid
+  end
 end
