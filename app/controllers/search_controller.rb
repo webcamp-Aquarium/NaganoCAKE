704aed7299c00d6d6a98c733e2e5ab695b69dafb
k-saunagi <k5.daha55@gmail.com>
2020-06-28 16:15:15 +0900

    カスタマー側にも検索機能を設置

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,6 @@
 class SearchController < ApplicationController
-  before_action :authenticate_admin_administrator!
-  layout 'admin'
   def search
-    if !Customer.search(params[:search]).blank?
-      # カスタマーで検索がヒットした場合
-      @customers = Customer.search(params[:search])
-    elsif !Product.search(params[:search]).blank?
+    if !Product.search(params[:search]).blank?
       # 商品で検索がヒットした場合
       @products = Product.search(params[:search])
     end
